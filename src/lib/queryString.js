8796461d31714ae38aaaca58b0792671c72f49fc
Ronaldo Faustino <ohnirucse@msn.com>
2023-07-29 18:08:40 -0300

    Refatoração

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,10 +1,25 @@
-module.exports.queryString = (obj) => {
-  console.log(Object.entries(obj));
-  const entries = Object.entries(obj).map((item) => {
-    console.log(item);
-    return `${item[0]}=${item[1]}`;
-  });
+module.exports.queryString = (obj) => 
+Object.entries(obj)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('$');
 
-  console.log(entries.join('$'));
-  return entries.join('$');
-};
+  ///////////////////////////1 Condigo antes da refatoração  
+  //{
+  //   console.log(Object.entries(obj));
+  //   const entries = Object.entries(obj).map((item) => {
+  //     console.log(item);
+  //     return `${item[0]}=${item[1]}`;
+  //   });
+
+  //   console.log(entries.join('$'));
+  //   return entries.join('$');
+  ////////////////////////////////////////////////////////
+
+  ///////////////////////////2 Condigo antes da refatoração
+//   return Object.entries(obj)
+//     .map((item) => {
+//         console.log(item);
+//         return `${item[0]}=${item[1]}`;
+//   }).join('$');
+  ////////////////////////////////////////////////////////
+//};
