630cd1fbb10f57984a1af4d2bab4aabfd9decfd8
Ronaldo Faustino <ohnirucse@msn.com>
2023-07-29 14:22:11 -0300

    Introdução de testes e watch

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -3,5 +3,28 @@ const {sum} = require('./calculator')
 it('should sum and 2 and the result must be 4', () =>{
     
     expect(sum(2,2)).toBe(4)
+
+})
+
+it('Should sum 2 and 2 even if one of them is a string and the result must be 4', () =>{
+    expect(sum('2','2')).toBe(4)
+})
+
+it('should throw an error if what is provided tothe method cannot be summed', () =>{
+    expect(()=>{
+        sum('','2')
+    }).toThrowError();
+
+    expect(()=>{
+        sum('2','2')
+    }).not.toThrowError();
+
+    expect(() =>{
+        sum({})
+    }).toThrowError();
+
+    expect(()=>{
+        sum()
+    }).toThrowError();
     
 })
