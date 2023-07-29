30783104c480b033519f1209d5939a48e507bf8a
Ronaldo Faustino <ohnirucse@msn.com>
2023-07-29 20:34:39 -0300

    Refatoração queryString

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,25 +1,32 @@
-module.exports.queryString = (obj) => 
-Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
+const KeyValueToString = ([key, value]) => {
+    if (typeof value === 'object' && !Array.isArray(value)) {
+      throw new Error('Please check yout params');
+    }
+    return `${key}=${value}`;
+  }
+
+module.exports.queryString = obj =>
+  Object.entries(obj)
+    .map(KeyValueToString)
     .join('$');
 
-  ///////////////////////////1 Condigo antes da refatoração  
-  //{
-  //   console.log(Object.entries(obj));
-  //   const entries = Object.entries(obj).map((item) => {
-  //     console.log(item);
-  //     return `${item[0]}=${item[1]}`;
-  //   });
+///////////////////////////1 Condigo antes da refatoração
+//{
+//   console.log(Object.entries(obj));
+//   const entries = Object.entries(obj).map((item) => {
+//     console.log(item);
+//     return `${item[0]}=${item[1]}`;
+//   });
 
-  //   console.log(entries.join('$'));
-  //   return entries.join('$');
-  ////////////////////////////////////////////////////////
+//   console.log(entries.join('$'));
+//   return entries.join('$');
+////////////////////////////////////////////////////////
 
-  ///////////////////////////2 Condigo antes da refatoração
+///////////////////////////2 Condigo antes da refatoração
 //   return Object.entries(obj)
 //     .map((item) => {
 //         console.log(item);
 //         return `${item[0]}=${item[1]}`;
 //   }).join('$');
-  ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
 //};
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -11,4 +11,27 @@ describe('Object ro query string', () => {
 
     expect(queryString(obj)).toBe('name=Ricardo$professor=develop');
   });
+
+  it('should create a valid query string even when as arrays is passed as values', () => {
+    const obj = {
+      name: 'Ricardo',
+      habiliadades: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Ricardo$habiliadades=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Ricardo',
+      habiliadades: {
+        primeiro: 'JS',
+        segundo: 'TDD',
+      },
+    };
+
+    expect(() => {
+        queryString(obj);
+    }).toThrowError();
+  });
 });
