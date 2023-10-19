9903f54f9d6891f9f94ca190e641eeaad2b9b106
Ronaldo Faustino <ohnirucse@msn.com>
2023-10-19 16:12:36 -0300

    Snapshot e Checkout

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -6,17 +6,43 @@ export default class Cart {
     items = []
 
     add(item){
-        const itemToFind = { product: item.product }
+        const itemToFind = { product: item.product };
+        
         if(find(this.items, itemToFind)){
-            remove(find(this.items, itemToFind));
+         remove(this.items, itemToFind);
         }
         this.items.push(item)
+    }
+
+    remove(product){
+        remove(this.items, {product})
     }
 
     getTotal(){
         return this.items.reduce((acc, item) =>{
             return acc + item.quantity * item.product.price;
         }, 0);
+    }
+
+    sumary() {
+        const total = this.getTotal();
+        const items = this.items;
+
+        return {
+            total,
+            items,
+        };
+    }
+
+    checkout() {
+        const { total , items } = this.sumary();
+
+        this.items = [];
+
+        return {
+            total,
+            items,
+        };
     }
 
 }
--- src/lib/cart.spec.js
+++ src/lib/cart.spec.js
@@ -5,40 +5,106 @@ describe('Cart', () =>{
 
     let product = {
         title: 'Adidas running shoes - me',
-        price: 35388
+        price: 35388,
+    }
+
+    let product2 = {
+        title: 'Mizuno running shoes - me',
+        price: 5000
     }
 
     beforeEach(()=> {
         cart = new Cart;
     });
 
-    it('should return 0 when getTotal() is executed in a newly created in instacia', () => {
-        expect(cart.getTotal()).toEqual(0)
+    describe('getTotal()', () =>{
+        it('should return 0 when getTotal() is executed in a newly created in instacia', () => {
+            expect(cart.getTotal()).toEqual(0)
+        })
+    
+        it('should multiply quantity an price and receive the total amount', () =>{
+            const item  = {
+                product,            
+                quantity: 2
+            };
+    
+            cart.add(item)
+    
+            expect(cart.getTotal()).toEqual(70776)
+        })
+    
+        it('should ensure no more than on product exist at time', () =>{
+            cart.add({
+                product,
+                quantity:2
+            })
+    
+            cart.add({
+                product,
+                quantity:1
+            })
+    
+            expect(cart.getTotal()).toEqual(35388)
+    
+        })
+    
+        it('should update total when a product gets included and removed', () => {
+            cart.add({
+                product: product2,
+                quantity:2
+            })
+    
+            cart.add({
+                product,
+                quantity:2
+            })        
+            cart.remove(product)
+    
+            expect(cart.getTotal()).toEqual(10000);
+        })
     })
 
-    it('should multiply quantity an price and receive the total amount', () =>{
-        const item  = {
-            product,            
-            quantity: 2
-        };
+    describe('checkout()', () => {
+        it('should return an object wih the total and the list of items', () =>{
+            cart.add({
+                product,
+                quantity:2
+            })
 
-        cart.add(item)
+            cart.add({
+                product: product2,
+                quantity:3
+            })
 
-        expect(cart.getTotal()).toEqual(70776)
-    })
-
-    it('should ensure no more than on product exist at time', () =>{
-        cart.add({
-            product,
-            quantity:2
+            expect(cart.checkout()).toMatchSnapshot();
         })
 
-        cart.add({
-            product,
-            quantity:1
+        it('should return an object wih the total and the list of items when sumary () is called ', () =>{
+            cart.add({
+                product,
+                quantity:2
+            })
+
+            cart.add({
+                product: product2,
+                quantity:3
+            })
+
+            expect(cart.sumary()).toMatchSnapshot();
+            expect(cart.getTotal()).toBeGreaterThan(0)
         })
 
-        expect(cart.getTotal()).toEqual(106164)
-
+        it('should reset the cart when chackout() is called', () => {
+            cart.add({
+                product: product2,
+                quantity:3
+            })
+    
+            cart.checkout();
+    
+            expect(cart.getTotal()).toEqual(0);
+        })
     })
+
+   
 })
